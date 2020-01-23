2f3f6977d40064a65a95152ba5c503aa7f27ed61
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-23 20:08:39 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,9 @@ require 'pry'
 def run_guessing_game
   #puts "Guess your own number between 1 and 6"
   random_num = rand(6) + 1
-  random_num.to_s
   user_input = gets.chomp
   
-  if user_input == random_num
+  if user_input.to_i == random_num
     puts "You guessed the correct number!"
   elsif user_input == "exit"
     puts "Goodbye!"
